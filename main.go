c9fcdfa1b384f8e062ebe37312e8be1be3c95a08
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-01-05 22:22:50 +0000

    destroying tf resources

--- main.go
+++ main.go
@@ -1,18 +1,66 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"path"
 )
 
-func main() {
-	directory := "/Users/rayhaan.bhikha/projects/acc-audit/scripts"
-	cmd := exec.Command("ls", "-l", directory)
-	cmd.Stdout = os.Stdout
-
-	err := cmd.Run()
+func handleErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func main() {
+
+	workspace := "260"
+	fmt.Println(workspace)
+	directory := "/Users/rayhaan.bhikha/projects/acc-audit/terraform/branch-builds"
+	modules := []string{"sqs", "lambda", "api", "graphql", "gateway", "frontend"}
+	autoApprove := true
+
+	for _, module := range modules {
+		destroyResource(workspace, module, directory, autoApprove)
+	}
+
+}
+
+func destroyResource(workspace, moduleToDelete, directory string, autoApprove bool) {
+	if moduleToDelete == "ecr" || moduleToDelete == "main" {
+		handleErr(fmt.Errorf("Should not destroy %s", moduleToDelete))
+	}
+
+	err := os.Chdir(path.Join(directory, moduleToDelete))
+	handleErr(err)
+
+	fmt.Println("destroying component ", moduleToDelete)
+	runTFCommand("init")
+	runTFCommand("workspace", "select", workspace)
+
+	vars := []string{
+		"-var",
+		"DRONE_BUILD_NUMBER=${DRONE_BUILD_NUMBER}",
+		"-var",
+		fmt.Sprintf("domain_prefix=bb-%s", workspace),
+	}
+	if autoApprove {
+		vars = append(vars, "-auto-approve")
+		args := append([]string{"destroy"}, vars...)
+		runTFCommand(args...)
+	} else {
+		vars = append(vars, "-destroy")
+		args := append([]string{"plan"}, vars...)
+		runTFCommand(args...)
+	}
+}
+
+func runTFCommand(args ...string) {
+	cmd := exec.Command("terraform", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	fmt.Println(err)
 }
