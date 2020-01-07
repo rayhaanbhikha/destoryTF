03481fa98c0ae3f7d03fb0fbf45eb7f64a8f4d32
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-01-07 22:02:00 +0000

    github token via env

--- .github/actions/upload-asset/index.js
+++ .github/actions/upload-asset/index.js
@@ -2,22 +2,14 @@ const fs = require('fs')
 const path = require('path')
 const core = require('@actions/core')
 const github = require('@actions/github')
-console.log("hello running a javascript action")
 
-
-console.log(process.env.GITHUB_TOKEN)
 console.log(fs.readdirSync(path.join(__dirname, "../../..")))
 
 const fileName = "destroyTF.darwin.tar.gz"
 
 async function run() {
-    // This should be a token with access to your repository scoped in as a secret.
-    // The YML workflow will need to set myToken with the GitHub Secret Token
-    // myToken: ${{ secrets.GITHUB_TOKEN }}
-    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-    const githubToken = core.getInput('GITHUB_TOKEN');
-    console.log(githubToken)
-    const octokit = new github.GitHub(githubToken);
+
+    const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
 
     // TODO: get values from env.
     const releaseTagResponse = await octokit.repos.getReleaseByTag({
