7bc8ae3336bd62058e3ec696ac9cb1d3f0d3562e
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-01-07 21:55:14 +0000

    get release upload_url

--- .github/actions/upload-asset/index.js
+++ .github/actions/upload-asset/index.js
@@ -8,6 +8,8 @@ console.log("hello running a javascript action")
 console.log(process.env.GITHUB_TOKEN)
 console.log(fs.readdirSync(path.join(__dirname, "../../..")))
 
+const fileName = "destroyTF.darwin.tar.gz"
+
 async function run() {
     // This should be a token with access to your repository scoped in as a secret.
     // The YML workflow will need to set myToken with the GitHub Secret Token
@@ -15,7 +17,26 @@ async function run() {
     // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
     const githubToken = core.getInput('GITHUB_TOKEN');
     console.log(githubToken)
-    // const octokit = new github.GitHub(githubToken);
+    const octokit = new github.GitHub(githubToken);
+
+    // TODO: get values from env.
+    const releaseTagResponse = await octokit.repos.getReleaseByTag({
+        owner: "rayhaanbhikha",
+        repo: "destroyTF",
+        tag: "v0.0.0"
+    })
+
+    console.log(releaseTagResponse)
+
+    // octokit.repos.uploadReleaseAsset({
+    //     file: fs.readFileSync(path.join(__dirname, "..", "..", "..", fileName)),
+    //     Headers: {
+    //         'content-type': 'application/zip'
+    //     },
+    //     name: fileName,
+    //     url: 
+    // })
+
 
     // const { data: pullRequest } = await octokit.pulls.get({
     //     owner: 'octokit',
