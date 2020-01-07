4cefdd2143085e6b04d905200ce853fd30522b39
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-01-07 22:32:02 +0000

    cleaned up

--- .github/actions/upload-asset/index.js
+++ .github/actions/upload-asset/index.js
@@ -3,8 +3,6 @@ const path = require('path')
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-console.log(fs.readdirSync(path.join(__dirname, "../../..")))
-
 const fileName = "destroyTF.darwin.tar.gz"
 const pathToFile = path.join(__dirname, "..", "..", "..", fileName)
 
@@ -14,26 +12,29 @@ function getFileSizeInBytes(pathToFile) {
 
 async function run() {
 
-    const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+    try {
+        const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
 
-    // TODO: get values from env.
-    const { data } = await octokit.repos.getReleaseByTag({
-        owner: "rayhaanbhikha",
-        repo: "destroyTF",
-        tag: "v0.0.0"
-    })
+        // TODO: get values from env.
+        const { data } = await octokit.repos.getReleaseByTag({
+            owner: "rayhaanbhikha",
+            repo: "destroyTF",
+            tag: "v0.0.0"
+        })
 
-    const response = await octokit.repos.uploadReleaseAsset({
-        file: fs.readFileSync(pathToFile),
-        headers: {
-            'content-type': 'application/zip',
-            'content-length': getFileSizeInBytes(pathToFile)
-        },
-        name: fileName,
-        url: data.upload_url
-    })
-
-    console.log(response)
+        await octokit.repos.uploadReleaseAsset({
+            file: fs.readFileSync(pathToFile),
+            headers: {
+                'content-type': 'application/zip',
+                'content-length': getFileSizeInBytes(pathToFile)
+            },
+            name: fileName,
+            url: data.upload_url
+        })
+    } catch (err) {
+        core.error(err)
+        core.setFailed(err.message)
+    }
 }
 
 run();
