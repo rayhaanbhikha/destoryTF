e76cdc66a9d3bf05158d4ad6f11088c0c49e7d51
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-01-07 21:21:33 +0000

    secret token test

--- .github/actions/upload-asset/index.js
+++ .github/actions/upload-asset/index.js
@@ -1,5 +1,32 @@
 const fs = require('fs')
 const path = require('path')
+const core = require('@actions/core')
+const github = require('@actions/github')
 console.log("hello running a javascript action")
 
+
+console.log(process.env.GITHUB_TOKEN)
 console.log(fs.readdirSync(path.join(__dirname, "../../..")))
+
+async function run() {
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    const githubToken = core.getInput('GITHUB_TOKEN');
+    console.log(githubToken)
+    // const octokit = new github.GitHub(githubToken);
+
+    // const { data: pullRequest } = await octokit.pulls.get({
+    //     owner: 'octokit',
+    //     repo: 'rest.js',
+    //     pull_number: 123,
+    //     mediaType: {
+    //       format: 'diff'
+    //     }
+    // });
+
+    // console.log(pullRequest);
+}
+
+run();
